da2d4af2438be4962d46f777a51e71bc05d43409
Ansley <anslet1@medtronic.com>
2020-01-22 09:11:50 -0600

    Updating from example app to Simpli

--- client/js/app.js
+++ client/js/app.js
@@ -1,33 +1,28 @@
-function getSessionList(success, error) {
-  var soql = "SELECT Session__r.Id, Session__r.Name FROM Session_Speaker__c";
+function getConfigList(success, error) {
+  var soql = "SELECT Id, Name, Class_Name__c,Description__c,Allow_Multiples__c,Parent_Object__c,SFDC_Object_API_Name__c, (SELECT Id, Name, Allow_Overrides__c, Allow_Multiples__c, External_Name__c, Is_Required__c, Type__c, Value__c, External_Index__c, Is_Formula__c, Formula__c, Is_Lookup__c, Lookup_Field__c, Lookup_Id_Value__c, Lookup_Return_Field__c FROM Simpli_Object__c.Simpli_Attributes__r) FROM Simpli_Object__c WHERE Is_Active__c = true ORDER BY Name";
   force.query(soql, success, error);
 }
 
-function getSessionDetails(sessionId, success, error) {
-  var soql = "SELECT Session__r.Name, " +
-  "Session__r.Session_Date__c, " +
-  "Speaker__r.First_Name__c, " +
-  "Speaker__r.Last_Name__c " +
-  "FROM Session_Speaker__c " +
-  "WHERE Session__r.Id = '" + sessionId + "'";
+function getConfigDetails(configId, success, error) {
+    var soql = "SELECT Id, Name, (SELECT Id, Name, Allow_Overrides__c, Allow_Multiples__c, External_Name__c, Is_Required__c, Type__c, Value__c, External_Index__c, Is_Formula__c, Formula__c, Is_Lookup__c, Lookup_Field__c, Lookup_Id_Value__c, Lookup_Return_Field__c FROM Simpli_Object__c.Simpli_Attributes__r) FROM Simpli_Object__c WHERE Id = '" + configId + "'";
   force.query(soql, success, error);
 }
 
-function showSessionList() {
-    getSessionList(
+function showConfigList() {
+    getConfigList(
         function (data) {
-            var sessions = data.records,
+            var configs = data.records,
                 html = '';
-            for (var i=0; i<sessions.length; i++) {
-                html += '<li class="table-view-cell"><a href="#sessions/'+ sessions[i].Session__r.Id +'">' + sessions[i].Session__r.Name + '</a></li>';
+            for (var i=0; i<configs.length; i++) {
+                html += '<li class="table-view-cell"><a href="#configs/'+ configs[i].Id +'">' + configs[i].Name + '</a></li>';
             }
             html =
                 '<div class="page">' +
                 '<header class="bar bar-nav">' +
-                    '<h1 class="title">Sessions</h1>' +
+                    '<h1 class="title">Configurations</h1>' +
                 '</header>' +
                 '<div class="content">' +
-                    '<ul class="table-view session-list">' + html + '</ul>' +
+                    '<ul class="table-view config-list">' + html + '</ul>' +
                 '</div>' +
                 '</div>';
             slider.slidePage($(html));
@@ -38,29 +33,29 @@ function showSessionList() {
     return false;
 }
 
-function showSessionDetails(sessionId) {
+function showConfigDetails(configId) {
 
-    getSessionDetails(sessionId,
+    getConfigDetails(configId,
         function (data) {
-            var session = data.records[0],
+            var config = data.records[0],
             html =
                 '<div class="page">' +
                 '<header class="bar bar-nav">' +
                 '<a class="btn btn-link btn-nav pull-left" href="#"><span class="icon icon-left-nav"></span>Back</a>' +
-            '<h1 class="title">Sessions</h1>' +
+            '<h1 class="title">Configurations</h1>' +
                 '</header>' +
                 '<div class="content">' +
                     '<div class="card">' +
                         '<ul class="table-view">' +
                             '<li class="table-view-cell">' +
-                                '<h4>' + session.Session__r.Name + '</h4>' +
-                                '<p>' + (session.Session__r.Session_Date__c || 'No time yet')+ '</p>' +
+                                '<h4>' + config.Name + '</h4>' +
+                                '<p>' + config.Class_Name__c + '</p>' +
                             '</li>' +
-                            '<li class="table-view-cell">Speaker: ' +
-                                session.Speaker__r.First_Name__c +
+                            '<li class="table-view-cell">SFDC Object: ' +
+                                config.SFDC_Object_API_Name__c +
                             '</li>' +
                             '<li class="table-view-cell">' +
-                                (session.Session__r.Description__c || 'No description yet') +
+                                (config.Description__c || 'No description') +
                             '</li>' +
                         '</ul>' +
                     '</div>' +
@@ -75,5 +70,5 @@ function showSessionDetails(sessionId) {
 }
 
 var slider = new PageSlider($('body')); // Initialize PageSlider micro-library for nice and hardware-accelerated page transitions
-router.addRoute('', showSessionList);
-router.addRoute('sessions/:id', showSessionDetails);
+router.addRoute('', showConfigList);
+router.addRoute('configs/:id', showConfigDetails);
