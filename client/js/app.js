c7b4dd5fd124486aaecca6c7704059f4b0526303
Ansley <anslet1@medtronic.com>
2020-01-23 12:15:14 -0600

    Same

--- client/js/app.js
+++ client/js/app.js
@@ -107,7 +107,7 @@ function showConfigDetails(configId) {
                 '<div class="page">' +
                 '<header class="bar bar-nav">' +
                 '<a class="btn btn-link btn-nav pull-left" href="#"><span class="icon icon-left-nav"></span>Back</a>' +
-                '<h1 class="title">Externally Named Attributes For ' + attribs[i].Simpli__Parent_Object__r.Name + '</h1>' +
+                '<h1 class="title">Externally Named Attributes</h1>' +
                 '</header>' +
                 '<div class="content">' +
                     '<div class="card">' +
